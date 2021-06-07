41de7e28772cd08e87df699bf4481652e9262ee1
Sam <sar325@cornell.edu>
2021-06-07 00:15:44 -0400

    Implement control center framework

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("keyup", function (event) {
+  if (event.key == "ArrowLeft") {
+    console.log("left");
+  }
+  if (event.key == "ArrowRight") {
+    console.log("right");
+  }
+  if (event.key == "ArrowUp") {
+    console.log("up");
+  }
+  if (event.key == "ArrowDown") {
+    console.log("down");
+  }
+});
